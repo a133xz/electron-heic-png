cddd7f182362fafbecf87b99ec8046829d7c4d5c
Antonio R <22479344+a133xz@users.noreply.github.com>
2021-06-22 13:01:54 +0100

    fix: exit and variables

--- src/main/index.js
+++ src/main/index.js
@@ -28,25 +28,59 @@ async function createWindow() {
 
   // Load the url of the dev server if in development mode
   // Load the index.html when not in development
-  win.loadURL(
-    isDevelopment
-      ? "http://localhost:3000"
-      : `file://${path.join(__dirname, "../renderer/index.html")}`
-  );
-  win.on("closed", () => (mainWindow = null));
+  win
+    .loadURL(
+      isDevelopment
+        ? "http://localhost:3000"
+        : `file://${path.join(__dirname, "../renderer/index.html")}`
+    )
+    .then(() => {
+      // Open Dev tools
+      isDevelopment && win.webContents.openDevTools();
+    })
+    .catch((error) => {
+      //log.error(error);
+    });
+
+  //win.on("closed", () => (win = null));
 }
 
-app.on("ready", createWindow);
+// Quit when all windows are closed.
 app.on("window-all-closed", () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== "darwin") {
     app.quit();
   }
 });
+
 app.on("activate", () => {
-  if (mainWindow === null) {
-    createWindow();
-  }
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on("ready", async () => {
+  createWindow();
+});
+
+// Exit cleanly on request from parent process in development mode.
+if (isDevelopment) {
+  if (process.platform === "win32") {
+    process.on("message", (data) => {
+      if (data === "graceful-exit") {
+        app.quit();
+      }
+    });
+  } else {
+    process.on("SIGTERM", () => {
+      app.quit();
+    });
+  }
+}
 
 // Events
 ipcMain.on("convertToHeic", (event, { filePath, fileName, outputFormat }) => {
