06e9e5aec44fbfd5a5e03b12497796b2d8502707
Antonio R <22479344+a133xz@users.noreply.github.com>
2021-07-30 21:33:31 +0200

    chore: vuejs updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,21 +4,14 @@ module.exports = {
     node: true,
     browser: true
   },
-  extends: [
-    "plugin:vue/recommended",
-    "eslint:recommended",
-    "prettier/vue",
-    "plugin:prettier/recommended",
-    '@vue/typescript'
-  ],
+  extends: ["plugin:vue/vue3-essential", "eslint:recommended"],
   rules: {
     "vue/component-name-in-template-casing": ["error", "PascalCase"],
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "semi": [2, "always"]
+    semi: [2, "always"]
   },
   parserOptions: {
     parser: "babel-eslint"
   }
 };
-//eee
--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,5 +1,5 @@
 import { createApp } from "vue";
-import App from "./App";
+import App from "./App.vue";
 import "./scss/main.scss";
 
 const app = createApp(App);
